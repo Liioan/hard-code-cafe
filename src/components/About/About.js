6d768fdbe39847cbe8f4dcbb0052607f1bc28c02
Liioan <dev.davidbijok@gmail.com>
2022-11-06 20:28:52 +0100

    Added content to <About/> component

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -1,3 +1,9 @@
+import { motion, animationPresence } from 'framer-motion';
+
+//. assets
+import Img from '../../assets/img/aboutUs.png';
+
+//. components
 import TypingText from '../TypingText/TypingText';
 
 //. styles
@@ -6,9 +12,23 @@ import styles from './About.module.css';
 export default function About() {
   return (
     <div className={styles.about} id='about'>
-      <h2>about</h2>
-      {/*  */}
-      {/*  */}
+      <motion.h2
+        className={styles.heading}
+        initial={{ opacity: 0 }}
+        whileInView={{ opacity: 1 }}
+        transition={{ duration: 0.5, ease: 'backOut', delay: 0.2 }}
+      >
+        <TypingText text='About us' />
+      </motion.h2>
+      <img src={Img} alt='' className={styles.img} />
+      <p className={styles.desc}>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Ad incidunt at,
+        error facilis natus vel eaque modi laudantium saepe voluptate quas
+        necessitatibus repellendus explicabo nisi commodi consequatur atque
+        vitae perspiciatis libero totam quod eligendi mollitia, ab voluptatibus?
+        Dignissimos cupiditate nobis possimus autem quas quis nulla atque
+        laborum perspiciatis labore. Laborum.
+      </p>
     </div>
   );
 }
