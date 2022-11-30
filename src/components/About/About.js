63f70098766acf4005d390b4c819f36b266cd918
Liioan <dev.davidbijok@gmail.com>
2022-11-30 19:10:23 +0100

    started creating responsivenes

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -18,29 +18,31 @@ export default function About() {
       >
         About Us
       </motion.h2>
-      <motion.img
-        src={Img}
-        alt=''
-        className={styles.img}
-        initial={{ translateX: '100%', opacity: 0 }}
-        whileInView={{ translateX: 0, opacity: 1 }}
-        viewport={{ once: true }}
-        transition={{ duration: 0.8, ease: 'anticipate', delay: 0.4 }}
-      />
-      <motion.p
-        className={styles.desc}
-        initial={{ translateX: '-100%', opacity: 0 }}
-        whileInView={{ translateX: 0, opacity: 1 }}
-        viewport={{ once: true }}
-        transition={{ duration: 0.8, ease: 'anticipate', delay: 0.6 }}
-      >
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Ad incidunt at,
-        error facilis natus vel eaque modi laudantium saepe voluptate quas
-        necessitatibus repellendus explicabo nisi commodi consequatur atque
-        vitae perspiciatis libero totam quod eligendi mollitia, ab voluptatibus?
-        Dignissimos cupiditate nobis possimus autem quas quis nulla atque
-        laborum perspiciatis labore. Laborum.
-      </motion.p>
+      <div className={styles.aboutContent}>
+        <motion.img
+          src={Img}
+          alt=''
+          className={styles.img}
+          initial={{ translateX: '100%', opacity: 0 }}
+          whileInView={{ translateX: 0, opacity: 1 }}
+          viewport={{ once: true }}
+          transition={{ duration: 0.8, ease: 'anticipate', delay: 0.4 }}
+        />
+        <motion.p
+          className={styles.desc}
+          initial={{ translateX: '-100%', opacity: 0 }}
+          whileInView={{ translateX: 0, opacity: 1 }}
+          viewport={{ once: true }}
+          transition={{ duration: 0.8, ease: 'anticipate', delay: 0.6 }}
+        >
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Ad incidunt
+          at, error facilis natus vel eaque modi laudantium saepe voluptate quas
+          necessitatibus repellendus explicabo nisi commodi consequatur atque
+          vitae perspiciatis libero totam quod eligendi mollitia, ab
+          voluptatibus? Dignissimos cupiditate nobis possimus autem quas quis
+          nulla atque laborum perspiciatis labore. Laborum.
+        </motion.p>
+      </div>
     </div>
   );
 }
--- src/components/Location/Location.js
+++ src/components/Location/Location.js
@@ -18,29 +18,31 @@ export default function Location() {
       >
         Location
       </motion.h2>
-      <motion.img
-        src={LocationImg}
-        alt='our location'
-        className={styles.img}
-        initial={{ translateX: '100%', opacity: 0 }}
-        whileInView={{ translateX: 0, opacity: 1 }}
-        viewport={{ once: true }}
-        transition={{ duration: 0.5, ease: 'anticipate', delay: 0.4 }}
-      />
-      <motion.div
-        className={styles.desc}
-        initial={{ translateX: '-100%', opacity: 0 }}
-        whileInView={{ translateX: 0, opacity: 1 }}
-        viewport={{ once: true }}
-        transition={{ duration: 0.5, ease: 'anticipate', delay: 0.4 }}
-      >
-        <h3 className={styles.subheader}>Come visit us!</h3>
-        <p className={styles.addres}>
-          <a href='https://www.google.com/maps/place/Ignacego+Kraszewskiego+11,+43-400+Cieszyn/@49.7412402,18.6324464,17z/data=!4m5!3m4!1s0x4714040fa566672d:0x59e236a224dca6b9!8m2!3d49.7409317!4d18.6328005'>
-            Our addres: Ignacego Kraszewskiego 11, 43-400 Cieszyn, Poland
-          </a>
-        </p>
-      </motion.div>
+      <div className={styles.locationContent}>
+        <motion.img
+          src={LocationImg}
+          alt='our location'
+          className={styles.img}
+          initial={{ translateX: '100%', opacity: 0 }}
+          whileInView={{ translateX: 0, opacity: 1 }}
+          viewport={{ once: true }}
+          transition={{ duration: 0.5, ease: 'anticipate', delay: 0.4 }}
+        />
+        <motion.div
+          className={styles.desc}
+          initial={{ translateX: '-100%', opacity: 0 }}
+          whileInView={{ translateX: 0, opacity: 1 }}
+          viewport={{ once: true }}
+          transition={{ duration: 0.5, ease: 'anticipate', delay: 0.4 }}
+        >
+          <h3 className={styles.subheader}>Come visit us!</h3>
+          <p className={styles.addres}>
+            <a href='https://www.google.com/maps/place/Ignacego+Kraszewskiego+11,+43-400+Cieszyn/@49.7412402,18.6324464,17z/data=!4m5!3m4!1s0x4714040fa566672d:0x59e236a224dca6b9!8m2!3d49.7409317!4d18.6328005'>
+              Our addres: Ignacego Kraszewskiego 11, 43-400 Cieszyn, Poland
+            </a>
+          </p>
+        </motion.div>
+      </div>
     </div>
   );
 }
